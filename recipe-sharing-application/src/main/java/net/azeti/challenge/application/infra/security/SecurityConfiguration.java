9ca2afe0f66b91283c215effe1513f666b0a0c74
ktikhonov <akirilltikhonov@gmail.com>
2023-06-05 23:00:43 +0300

    [AZT-1] add basic spring security

--- recipe-sharing-application/src/main/java/net/azeti/challenge/application/infra/security/SecurityConfiguration.java
+++ recipe-sharing-application/src/main/java/net/azeti/challenge/application/infra/security/SecurityConfiguration.java
@@ -1,16 +1,43 @@
-//package net.azeti.challenge.application.infra.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfiguration {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        // We disabled csrf for simplicity.
-//        return http.csrf().disable().build();
-//    }
-//}
+package net.azeti.challenge.application.infra.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // We disabled csrf for simplicity.
+        return http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated().and()
+                .httpBasic().and()
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(UserDetailsService.class)
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User
+                        .withUsername("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
